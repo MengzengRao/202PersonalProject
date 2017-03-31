8a37c1a4bc42d253b921b7d201b26c42d0343fa1
mengzeng rao <mengzeng rao>
2017-03-31 01:56:28 -0700

    something

---
+++ UmlProject/cmpe202-UML_PROJECT/diagram/UMLImageRender.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package diagramGenerator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import net.sourceforge.plantuml.SourceStringReader;
+import parser.MethodDeclarationStructure;
+import parser.ParsedClass;
+
+/**
+ * @author mengzeng
+ *
+ */
+
+public class UMLImageRender {
+	
+	public void createClassUMLObject(ArrayList<ParsedClass> pc, String diagramGeneratorName) throws IOException{
+		ParseString.clearParseStringMethods();
+		if(diagramGeneratorName.equals("plantuml")) {
+			ParseString.clearParseStrings();
+			PlantUMLDiagramCodeGenerator plantUML = new PlantUMLDiagramCodeGenerator();
+			for(ParsedClass parsedClass : pc) {
+				plantUML.resetClassFields();
+				plantUML.setClassFields(parsedClass.getPackageName(), parsedClass.getModifierName(), parsedClass.getClassType(), parsedClass.getClassName(), parsedClass.getAttributeArray(), parsedClass.getMethodsArray(),parsedClass.getDependencyList(),parsedClass.getConstructors());
+				plantUML.generateUMLParsedCode();
+			}
+			plantUML.renderUMLDiagram(pc);
+		}
+	}
+}
