03ad59a075970bd49411ad21af7f56723219ae3b
mengzeng rao <mengzeng rao>
2017-04-01 20:12:43 -0700

    class

--- UmlProject/cmpe202-UML_PROJECT/parser/class.java
+++ UmlProject/cmpe202-UML_PROJECT/parser/class.java
@@ -17,3 +17,26 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import mainCom.ParseString;
 import utilities.UtilitiesFunctions;
+public final class Class {
+
+	private static Map<String, String> accessModifierMap = new HashMap<>();
+	private static Map<String, String> accessModifierCoveterMap = new HashMap<>();
+	private static Map<String, String> dataTypesMap = new HashMap<>();
+	private static Map<String, String> otherModifierMap = new HashMap<>();
+	private static ArrayList<MethodDeclarationStructure> methodsArray;
+	private static ArrayList<MethodDeclarationStructure> constructorArray;
+	private static ArrayList<VariableDeclarationStructure> attributesArray;
+	private static String variableAccessModifier;
+	private static String[] variableType;
+	private static String variableName;
+	private static ArrayList<String> methodAttributes = new ArrayList<String>();
+	private static ArrayList<String> otherVariableModifiers;
+	private static  Map<String, String> allClassNames;
+	//private static final String[] accessModifiersConverter = { "1;public", "2;private", "5;protected","0;package" };
+	private static final String[] accessModifiers = { "public;public", "private;private", "protected;protected","package;package" };
+	private static final String[] dataTypes = { "int;int", "byte;byte", "short;short", "long;long", "float;float","double;double", "boolean;boolean", "char;char", "String;String","Map;Map","List;List","ArrayList;ArrayList" };
+	private static final String[] otherModifiers = {"static;static","synchronized;synchronized","final;final","abstract;abstract"};
+	
+	private ClassAttributesParser() {
+	}
+}	
