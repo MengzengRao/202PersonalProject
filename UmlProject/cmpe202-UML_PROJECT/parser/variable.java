5299bee5f8b7f2f1d704bb92b677d937c3506462
MengzengRao <mengzeng.rao@sjsu.edu>
2017-04-08 18:12:13 -0700

    Add files via upload
    variable

---
+++ UmlProject/cmpe202-UML_PROJECT/parser/variable.java
@@ -0,0 +1,15 @@
+package umlProject;
+
+import java.util.ArrayList;
+
+public class variable {
+	private String variableAccessModifier;
+	private String variableType;
+	private String variableName;
+	
+	public variable(String variableAccessModifier, String variableType, String variableName) {
+		this.variableAccessModifier = variableAccessModifier;
+		this.variableType = variableType;
+		this.variableName = variableName;
+	}
+}
