b5f12d029951c17ab290a4576a20d7affb012470
MengzengRao <mengzeng.rao@sjsu.edu>
2017-04-15 18:53:30 -0700

    end of Friday work

---
+++ UmlProject/cmpe202-UML_PROJECT/parser/ParsedClass.java
@@ -0,0 +1,70 @@
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import mainCom.ParseString;
+
+
+
+public class ParsedClass {
+	private String packageName;
+	private String modifierName;
+	private String classType;
+	private String className;
+	private Map<String, java.util.ArrayList<String>> dependencyList = new HashMap<>();
+	private ArrayList<String> AttributeArray;
+	private ArrayList<MethodDeclarationStructure> MethodsArray;
+	private ArrayList<MethodDeclarationStructure> constructorArray;
+
+	public ParsedClass(String packageName, String modifierName, String classType, String className,
+			Map<String, ArrayList<String>> dependencyList, ArrayList<String> attributeArray,
+			ArrayList<MethodDeclarationStructure> methodsArray,ArrayList<MethodDeclarationStructure> constructorArray) {
+		if (packageName == "" || packageName == null)
+			this.packageName = "DefaultPackage";
+		else
+			this.packageName = packageName;
+		this.modifierName = modifierName;
+		this.classType = classType;
+		this.className = className;
+		this.dependencyList.putAll(dependencyList);
+		this.AttributeArray = attributeArray;
+		this.MethodsArray = methodsArray;
+		this.constructorArray = constructorArray;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public String getModifierName() {
+		return modifierName;
+	}
+
+	public String getClassType() {
+		return classType;
+	}
+
+	public Map<String, java.util.ArrayList<String>> getDependencyList() {
+		return dependencyList;
+	}
+
+	public ArrayList<String> getAttributeArray() {
+		return AttributeArray;
+	}
+
+	public ArrayList<MethodDeclarationStructure> getMethodsArray() {
+		return MethodsArray;
+	}
+
+	public ArrayList<MethodDeclarationStructure> getConstructors() {
+		return constructorArray;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	
+}
